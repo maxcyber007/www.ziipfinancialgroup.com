aab7bb44b2084c8168acdfbb3e9cc2296bdb384a
maxcyber007 <rungsiyanon@gmail.com>
2023-02-16 19:42:01 +0700

    version 2.0

--- next.config.js
+++ next.config.js
@@ -3,4 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  nextConfig,
+  experimental: {
+    images: {
+      unoptimized: true,
+    },
+  },
+}
