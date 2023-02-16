fd0ef74d2eba700fa3d5486bd3b0e405b5ad49c8
maxcyber007 <rungsiyanon@gmail.com>
2023-02-16 19:04:47 +0700

    version 1.0

--- pages/_app.js
+++ pages/_app.js
@@ -41,15 +41,15 @@ export default function App({ Component, pageProps }) {
       <Component {...pageProps} />
 {/* <Footer /> */}
       {/* <!-- Vendor JS Files --> */}
-      <script src="/assets/vendor/purecounter/purecounter_vanilla.js"></script>
-      <script src="/assets/vendor/aos/aos.js"></script>
-      <script src="/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-      <script src="/assets/vendor/glightbox/js/glightbox.min.js"></script>
-      <script src="/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-      <script src="/assets/vendor/swiper/swiper-bundle.min.js"></script>
-      <script src="/assets/vendor/php-email-form/validate.js"></script>
+      <Script src="/assets/vendor/purecounter/purecounter_vanilla.js" />
+      <Script src="/assets/vendor/aos/aos.js" />
+      <Script src="/assets/vendor/bootstrap/js/bootstrap.bundle.min.js" />
+      <Script src="/assets/vendor/glightbox/js/glightbox.min.js" />
+      <Script src="/assets/vendor/isotope-layout/isotope.pkgd.min.js" />
+      <Script src="/assets/vendor/swiper/swiper-bundle.min.js" />
+      <Script src="/assets/vendor/php-email-form/validate.js" />
       {/* <!-- Template Main JS File --> */}
-      <script src="/assets/js/main.js"></script>
+      <Script src="/assets/js/main.js" />
     </>
   )
 }
