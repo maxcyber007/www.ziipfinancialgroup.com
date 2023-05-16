ac3c77b97f40c202fb14fdf5a46b690e4f6d6e5a
maxcyber007 <rungsiyanon@gmail.com>
2023-05-16 16:23:23 +0700

    160120231623

--- pages/index.js
+++ pages/index.js
@@ -33,7 +33,7 @@ export default function Home() {
               </div>
             </div>
             <div className="col-lg-6 hero-img" data-aos="zoom-out" data-aos-delay={200}>
-              <Image src="/assets/img/img-mobile1.png" className="img-fluid" width={439} height={338} alt="img-mobile1" />
+              <Image src="/assets/img/img-mobile.png" className="img-fluid" width={439} height={338} alt="img-mobile1" />
             </div>
           </div>
         </div>
