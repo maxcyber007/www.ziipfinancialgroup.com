c5884a6c6158e2d90099df042c2b548d504647ab
maxcyber007 <rungsiyanon@gmail.com>
2023-05-17 18:24:07 +0700

    170520231824

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ export default function Home() {
       <section id="hero" className="hero d-flex align-items-center">
         <div className="container">
           <div className="row">
-            <div className="col-lg-6 d-flex flex-column justify-content-center">
+            <div className="col-lg-6 d-flex flex-column justify-content-center order-md-first order-last">
               <h1 data-aos="fade-up">Ziip Financial Group Company Limited</h1>
               <h2 data-aos="fade-up" data-aos-delay={400}>Estate investment, Fin Tech, E-Commerce<br /><span className="text-gray">That is committed to providing investment opportunities Safe and legal to customers.</span></h2>
               <div data-aos="fade-up" data-aos-delay={600}>
@@ -32,7 +32,7 @@ export default function Home() {
                 </div>
               </div>
             </div>
-            <div className="col-lg-6 hero-img" data-aos="zoom-out" data-aos-delay={200}>
+            <div className="col-lg-6 hero-img order-md-last order-first" data-aos="zoom-out" data-aos-delay={200}>
               <Image src="/assets/img/img-mobile.png" className="img-fluid" width={509} height={408} alt="img-mobile1" />
             </div>
           </div>
